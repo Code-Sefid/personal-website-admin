a99e39d964209a7b6e5022ad6dd1012935d93868
soheil <soheilkhaledabdi@gmail.com>
2023-11-11 17:01:34 +0330

    Update -> write route better and add new routes for articles

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Users\All as UsersAll;
+use App\Livewire\Articles\All as ArticlesAll;
+use App\Livewire\Articles\Add as ArticlesAdd;
+use App\Livewire\Articles\Edit as ArticlesEdit;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::view('/' , 'welcome');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-    Route::get("users",\App\Livewire\Users\All::class)->name('admin.users');
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('users', UsersAll::class)->name('admin.users');
+    Route::get('articles', ArticlesAll::class)->name('admin.articles');
+    Route::get('article/create', ArticlesAdd::class)->name('admin.articles.create');
+    Route::get('article/update/{id}',ArticlesEdit::class)->name('admin.articles.update');
 });
